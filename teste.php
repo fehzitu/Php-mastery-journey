b4a1b3ab15618dd436b775266a9e612ffebac9bc
fehzitu <fehzitujsdev@gmail.com>
2026-03-18 17:34:07 -0300

    adding context to php introduction

--- teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // comentário em linha unica
-    /*
-        comentário multi linhas
-        comentário multi linhas
-        comentário multi linhas
-    */
-    # comentário em linha unica
-?>
