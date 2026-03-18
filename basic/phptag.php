40038b7f808cc009522782e1506069bad3cc62f5
fehzitu <fehzitujsdev@gmail.com>
2026-03-18 18:52:17 -0300

    starting the basics

---
+++ basic/phptag.php
@@ -0,0 +1,9 @@
+<?php
+    // print is a to print on console
+    print "See php super tag like an php code block <?php { code here } ?>, and all php comments are simple like C and C++ or unix";
+    // single line comment
+    /*
+        block comment, multi line comment
+    */
+    # single line comment too
+?>
