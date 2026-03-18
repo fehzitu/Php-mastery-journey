4b8a1466d58cac5cd104a138f12cbe55be8a7ae2
fehzitu <fehzitujsdev@gmail.com>
2026-03-18 18:56:33 -0300

    add content to the basics

--- basic/phptag.php
+++ basic/phptag.php
@@ -1,9 +1,15 @@
 <?php
-    // print is a to print on console
-    print "See php super tag like an php code block <?php { code here } ?>, and all php comments are simple like C and C++ or unix";
-    // single line comment
-    /*
-        block comment, multi line comment
-    */
-    # single line comment too
+// print or echo is a to print on console
+print "See php super tag like an php code block <?php { code here } ?>";
+echo "and all php comments are simple like C and C++ or unix";
+
+# single line comment
+// single line comment too
+/*
+    block comment, multi lines comment
+*/
+
+/*
+Imagine that the tag <?php?> functions as a super tag where we place all our PHP code. This is useful for placing PHP within other locations in a more organized and easier-to-understand way.
+*/
 ?>
