37c3c2d84f1d5e1e7b30c0230c9c5f8dda537ec2
fehzitu <fehzitujsdev@gmail.com>
2026-03-18 18:58:54 -0300

    add links

--- basic/phptag.php
+++ basic/phptag.php
@@ -12,4 +12,7 @@ echo "and all php comments are simple like C and C++ or unix";
 /*
 Imagine that the tag <?php?> functions as a super tag where we place all our PHP code. This is useful for placing PHP within other locations in a more organized and easier-to-understand way.
 */
+
+// About php super tag: https://www.php.net/manual/en/language.basic-syntax.phptags.php
+// About comments: https://www.php.net/manual/en/language.basic-syntax.comments.php
 ?>
