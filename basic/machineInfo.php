76d2041596a5cfa2960bc32a689ad0d63778136c
fehzitu <fehzitujsdev@gmail.com>
2026-03-22 22:38:35 -0300

    studing much, write less xD

---
+++ basic/machineInfo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Info</title>
+</head>
+<body>
+<!-- we can use php inside an html using a php tag -->
+    <h1>
+        <?php
+        // using "phpinfo" function we can show the server info on a website
+            phpinfo();
+        ?>
+    </h1>
+</body>
+</html>
